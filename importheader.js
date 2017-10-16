87e940309ff6bf28f9274285de9a4127a65a6f27
Niklas Lindblom-Hertz <niklas.lindblomhertz@lund.ntig.se>
2017-10-16 11:03:38 +0200

    Added other sites in header. Header is added through javascript.

---
+++ importheader.js
@@ -0,0 +1,42 @@
+
+//ändra bara här om du ska ändra headern!!!
+document.write('\
+\
+		<header>                                                                \
+		<div class ="main-page-header">                                         \
+			<h1>                                                                \
+				Bibliomania                                                     \
+				<img class="img-header" src="books.png" alt="Mainpicture">      \
+			</h1>                                                               \
+		</div>                                                                  \
+		<div class="navigationbar">                                             \
+			<table>                                                             \
+				<th>                                                            \
+					<a class="homepage" href="index.php">                       \
+						Home                                                    \
+					</a>                                                        \
+				</th>                                                           \
+				<th>                                                            \
+				                                                                \
+					<a class = "allbooks" href = "allbooks.html">All books</a>  \
+				</th>                                                           \
+				<th>                                                            \
+				<a class = "newbooks" href = "newbooks.html">                   \
+					New books                                                   \
+					</a>                                                        \
+				</th>                                                           \
+				<th>                                                            \
+				<a class = "categories" href = "categories.html">               \
+					Categories                                                  \
+					</a>                                                        \
+				</th>                                                           \
+				<th>                                                            \
+				<a  class = "borrowbooks" href = "categories.html">             \
+					Borrow books                                                \
+					</a>                                                        \
+				</th>                                                           \
+			</table>                                                            \
+		</div>                                                                  \
+		</header>                                                               \
+		                                                                        \
+		');
