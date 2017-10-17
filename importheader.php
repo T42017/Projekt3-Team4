4bb4ccd26c4bab9fde56e2156244e67a56c10b38
Niklas Lindblom-Hertz <niklas.lindblomhertz@lund.ntig.se>
2017-10-17 09:55:36 +0200

    Header is now fixed with all css and links. Adding search bar

---
+++ importheader.php
@@ -0,0 +1,36 @@
+		<header>                                                              
+		<div class ="main-page-header">                                       
+			<h1>                                                              
+				Bibliomania                                                   
+				<img class="img-header" src="books.png" alt="Mainpicture">    
+			</h1>                                                             
+		</div>                                                                
+		<div class="navigationbar">                                           
+			<table>                                                           
+				<th>                                                          
+					<a class="homepage" href="index.php">                     
+						Home                                                  
+					</a>                                                      
+				</th>                                                         
+				<th>                                                          
+				                                                              
+					<a class = "allbooks" href = "allbooks.php">All books</a>
+				</th>                                                         
+				<th>                                                          
+				<a class = "newbooks" href = "newbooks.php">                 
+					New books                                                 
+					</a>                                                      
+				</th>                                                         
+				<th>                                                          
+				<a class = "categories" href = "categories.php">             
+					Categories                                                
+					</a>                                                      
+				</th>                                                         
+				<th>                                                          
+				<a  class = "borrowbooks" href = "borrowbooks.php">           
+					Borrow books                                              
+					</a>                                                      
+				</th>                                                         
+			</table>                                                          
+		</div>                                                                
+		</header>                                                             
